87a112866ec0d6f6f249e00d187bc357a80e064c
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-08-27 21:08:49 +0600

    twd config fixed

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ import { Drash } from "https://deno.land/x/drash@v1.5.1/mod.ts";
 import HomeResource from "./home_resource.js";
 
 const server = new Drash.Http.Server({
+  directory: Deno.cwd(),
   response_output: "text/html",
   resources: [
     HomeResource,
-  ]
+  ],
+  static_paths: ["/public"]
 });
 
 console.log("Server is running on port 1447");
