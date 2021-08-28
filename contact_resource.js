4045243523421d77e065bba38e5c7c054dd0d360
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-08-28 20:29:00 +0600

    Contact Resource added

---
+++ contact_resource.js
@@ -0,0 +1,24 @@
+import { Drash } from "https://deno.land/x/drash@v1.5.1/mod.ts";
+
+const decoder = new TextDecoder();
+
+export default class ContactResource extends Drash.Http.Resource {
+
+  static paths = [
+    "/contact"
+  ];
+
+  GET() {
+    try {
+      let fileContentsRaw = Deno.readFileSync("./contact.html");
+      let template = decoder.decode(fileContentsRaw);
+      this.response.body = template;
+    } catch (error) {
+      throw new Drash.Exceptions.HttpException(
+        400,
+        `Error reading HTML template.`
+      );
+    }
+    return this.response;
+  }
+}
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import { Drash } from "https://deno.land/x/drash@v1.5.1/mod.ts";
 
 import HomeResource from "./home_resource.js";
+import ContactResource from "./contact_resource.js";
 
 const server = new Drash.Http.Server({
   directory: Deno.cwd(),
   response_output: "text/html",
   resources: [
     HomeResource,
+    ContactResource,
   ],
   static_paths: ["/public"]
 });
