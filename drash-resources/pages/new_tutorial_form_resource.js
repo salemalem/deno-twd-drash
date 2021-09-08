fbe8ad202f9abe34ebf5b268c149643d4212ba60
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-09-08 22:59:55 +0600

    slugify is separated as utilit

--- drash-resources/pages/new_tutorial_form_resource.js
+++ drash-resources/pages/new_tutorial_form_resource.js
@@ -2,19 +2,8 @@
 import { Drash } from "../../dependencies.js";
 import { tutorialsDB } from "../../database/database.js";
 import { decodeQueryParam } from "../../utilities/decode_query_param.js";
+import { slugify } from "../../utilities/slugify.js";
 
-// source: https://stackoverflow.com/questions/1053902/how-to-convert-a-title-to-a-url-slug-in-jquery
-function slugify(string) {
-  return string
-    .toString()
-    .trim()
-    .toLowerCase()
-    .replace(/\s+/g, "-")
-    .replace(/[^\w\-]+/g, "")
-    .replace(/\-\-+/g, "-")
-    .replace(/^-+/, "")
-    .replace(/-+$/, "");
-}
 export class NewTutorialFormResource extends Drash.Http.Resource {
 
   static paths = [
---
+++ utilities/slugify.js
@@ -0,0 +1,12 @@
+// source: https://stackoverflow.com/questions/1053902/how-to-convert-a-title-to-a-url-slug-in-jquery
+export function slugify(string) {
+  return string
+    .toString()
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w\-]+/g, "")
+    .replace(/\-\-+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "");
+}
