4f5db120d4c8d98ca531ebce943a94818385b15e
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-09-11 23:39:53 +0600

    render is used instead of compile

--- ship_to_dist.js
+++ ship_to_dist.js
@@ -1,5 +1,9 @@
 import * as Eta from "https://deno.land/x/eta@v1.12.3/mod.ts";
 
 var myTemplate = "Hi, my name is <%= it.name %>"
-var compiled = Eta.compile(myTemplate)
-console.log(compiled({ name: "Johnny Appleseed" }, Eta.config))
+// var compiled = Eta.compile(myTemplate)
+// console.log(compiled({ name: "Johnny Appleseed" }, Eta.config))
+var rendered = Eta.render(myTemplate, { 
+  name: "Johnny Appleseed" 
+});
+console.log(rendered);
