3c977670f2ef40af43dafa7e5974109159000a86
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-09-12 10:04:49 +0600

    home resource post request to ship to dist added

--- drash-resources/home_resource.js
+++ drash-resources/home_resource.js
@@ -1,36 +1,76 @@
 // Index page
 import { Drash } from "../dependencies.js";
+
 export class HomeResource extends Drash.Http.Resource {
   static paths = ["/"];
 
-    async GET() {
-      try {
-        this.response.body = await this.response.render(
-          Deno.cwd() + "/public/views/index",
-          {
-            page_title: "Home",
-            tutorials: [
-              {
-                title: "How to install Linux",
-                description: "A tutorial on how to install Linux on your computer.",
-                slug: "how-to-install-linux",
-                date: new Date().toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: '2-digit'}),
-              },
-              {
-                title: "How to install Windows",
-                description: "A tutorial on how to install Windows on your computer.",
-                slug: "how-to-install-windows",
-                date: new Date().toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: '2-digit'}),
-              },
-              {
-                title: "How to install ChromiumOS",
-                description: "A tutorial on how to install ChromiumOS on your computer.",
-                slug: "how-to-install-chromiumos",
-                date: new Date().toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: '2-digit'}),
-              },
-            ]
-          },
-        );
+  async GET() {
+    try {
+      this.response.body = await this.response.render(
+        Deno.cwd() + "/public/views/index",
+        {
+          page_title: "Home",
+          tutorials: [
+            {
+              title: "How to install Linux",
+              description: "A tutorial on how to install Linux on your computer.",
+              slug: "how-to-install-linux",
+              date: new Date().toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: '2-digit'}),
+            },
+            {
+              title: "How to install Windows",
+              description: "A tutorial on how to install Windows on your computer.",
+              slug: "how-to-install-windows",
+              date: new Date().toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: '2-digit'}),
+            },
+            {
+              title: "How to install ChromiumOS",
+              description: "A tutorial on how to install ChromiumOS on your computer.",
+              slug: "how-to-install-chromiumos",
+              date: new Date().toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: '2-digit'}),
+            },
+          ]
+        },
+      );
+  } catch (error) {
+      throw new Drash.Exceptions.HttpException(
+        400,
+        `Error reading ETA template.`
+      );
+    }
+    return this.response;
+  }
+
+  async POST() {
+    try {
+      let renderedHtmlOutput = await this.response.render(
+        Deno.cwd() + "/public/views/index",
+        {
+          page_title: "Home",
+          tutorials: [
+            {
+              title: "How to install Linux",
+              description: "A tutorial on how to install Linux on your computer.",
+              slug: "how-to-install-linux",
+              date: new Date().toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: '2-digit'}),
+            },
+            {
+              title: "How to install Windows",
+              description: "A tutorial on how to install Windows on your computer.",
+              slug: "how-to-install-windows",
+              date: new Date().toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: '2-digit'}),
+            },
+            {
+              title: "How to install ChromiumOS",
+              description: "A tutorial on how to install ChromiumOS on your computer.",
+              slug: "how-to-install-chromiumos",
+              date: new Date().toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: '2-digit'}),
+            },
+          ]
+        },
+      );
+      Deno.writeTextFileSync(`${Deno.cwd()}/dist/index.html`, renderedHtmlOutput);
+      this.response.body = "successfully rendered file!";
     } catch (error) {
       throw new Drash.Exceptions.HttpException(
         400,
--- ship_to_dist.js
+++ ship_to_dist.js
@@ -1,9 +1,30 @@
 import * as Eta from "https://deno.land/x/eta@v1.12.3/mod.ts";
 
-var myTemplate = "Hi, my name is <%= it.name %>"
-// var compiled = Eta.compile(myTemplate)
-// console.log(compiled({ name: "Johnny Appleseed" }, Eta.config))
-var rendered = Eta.render(myTemplate, { 
-  name: "Johnny Appleseed" 
+
+// TODO
+// RESEARCH how to compile a file not to parse it to string
+const indexEtaText = await Deno.readTextFile(Deno.cwd() + "/public/views/index.eta");
+var rendered = Eta.render(indexEtaText, {
+  page_title: "Home",
+  tutorials: [
+    {
+      title: "How to install Linux",
+      description: "A tutorial on how to install Linux on your computer.",
+      slug: "how-to-install-linux",
+      date: new Date().toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: '2-digit'}),
+    },
+    {
+      title: "How to install Windows",
+      description: "A tutorial on how to install Windows on your computer.",
+      slug: "how-to-install-windows",
+      date: new Date().toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: '2-digit'}),
+    },
+    {
+      title: "How to install ChromiumOS",
+      description: "A tutorial on how to install ChromiumOS on your computer.",
+      slug: "how-to-install-chromiumos",
+      date: new Date().toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: '2-digit'}),
+    },
+  ]
 });
 console.log(rendered);
